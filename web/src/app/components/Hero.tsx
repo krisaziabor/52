cc7fbd66b8d09a593458065ef2d9eeac4078fdcd
kris aziabor <krisaaziabor@gmail.com>
2025-03-23 14:13:21 -0400

    fixing small copy issues

--- web/src/app/components/Hero.tsx
+++ web/src/app/components/Hero.tsx
@@ -14,10 +14,9 @@ export default function Bio() {
       />
       <div className="flex flex-col w-full sm:w-1/2 gap-4 text-sm sm:text-base text-foreground">
         <p>
-          A site dedicated to my efforts of becoming a person of habit and
-          someone who has traditions for themself.
+        A site dedicated to my efforts to become a person of habit and someone who has traditions for themselves. 
         </p>
-        <p>A home for the excitement I always have for freshly new projects.</p>
+        <p>A home for the excitement I always have for brand new projects. </p>
         <p>An archive of my weekly creations for the next 52.</p>
       </div>
     </div>
